b8ddb8255750a3fc3f44a1956ebfcf7be9cfe6ac
Thais <rotinasoftware@gmail.com>
2022-08-08 10:10:02 -0300

    docs: Adição de docstrings

--- utils.py
+++ utils.py
@@ -2,13 +2,14 @@ import camelot
 import pandas as pd
 
 class ContentExtractor:
-    """Classe para extrair dados do histórico"""
+    """Class to extract historic data"""
     def __init__(self, historic):
         self._historic = historic
         self.aproved_courses = []
         self.extract_tables()
 
     def _is_a_done_table(self, table):
+        """Returns True if a table is a done table"""
         pandas_table = table.df
         if len(pandas_table.columns) == 9:
             return True
@@ -16,11 +17,13 @@ class ContentExtractor:
             return False
 
     def _add_to_done_table(self, table):
+        """Save aproved courses in a list"""
         for i, linha in table.df.iterrows():
             if 'APR' in linha[8]:
                 self.aproved_courses.append(linha[2])
 
     def extract_tables(self):
+        """Extract historic tables"""
         tables = camelot.read_pdf(self._historic, pages='all')
 
         for index, table in enumerate(tables):
