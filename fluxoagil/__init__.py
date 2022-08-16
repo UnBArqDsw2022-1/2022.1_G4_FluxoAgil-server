ceb2515554c8a15b338f94965359923fcdf2ee18
lbvx <lucasbraunxav@gmail.com>
2022-08-16 20:45:44 -0300

    feat: adiciona config MAX_CONTENT_LENGTH

--- fluxoagil/__init__.py
+++ fluxoagil/__init__.py
@@ -12,6 +12,7 @@ def create_app():
     app = Flask(__name__)
     api = Api(app)
     app.config['SECRET KEY'] = ''
+    app.config['MAX_CONTENT_LENGTH'] = 512 * 1024   # limits maximum allowed upload size to 512KiB
     api.add_resource(HelloFluxoAgil, '/')
     api.add_resource(AcademicHistory, '/academic-history')
     return app
