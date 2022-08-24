9646b7ef3466fbfedf2241a86b9a33e14cefbc38
Thais <rotinasoftware@gmail.com>
2022-08-24 16:01:52 -0300

    feat: Extração do numero do curriculo no historico

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import camelot
+import fitz
+import re
 import pandas as pd
 
 class ContentExtractor:
@@ -7,6 +9,8 @@ class ContentExtractor:
         self._historic = historic
         self.aproved_courses = []
         self.extract_tables()
+        self.curriculun_number = ''
+        self.extract_curriculum()
 
     def _is_a_done_table(self, table):
         """Returns True if a table is a done table"""
@@ -29,3 +33,23 @@ class ContentExtractor:
         for index, table in enumerate(tables):
             if self._is_a_done_table(table):
                 self._add_to_done_table(table)
+
+    def find_curriculum(self, text):
+        """
+        Prepare the text for extration and
+        find curriculum number using regex
+        """
+        blocks = re.split('\n\n', text)
+        # 
+        for block in blocks:
+            pattern = r"\d+/-?\d -"
+            match = re.findall(pattern, block)
+            match = "".join(match)
+            self.curriculun_number = match.replace('-', '')
+            return self.curriculun_number
+
+    def extract_curriculum(self):
+        """Read first page text to extract curriculum"""
+        doc = fitz.open(self._historic)  
+        text = doc[0].get_text()
+        text = self.find_curriculum(text)
