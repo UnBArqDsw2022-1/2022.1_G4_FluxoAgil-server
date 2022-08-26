19a58b5c145e7ba264be1aebfbd98dfe8b4e0a4f
Yudi Yamane <yudiyamane@gmail.com>
2022-08-26 08:26:25 -0300

    feat: adiciona tabela de disciplinas e relacionamento entre disciplina e currículo

--- fluxoagil/models.py
+++ fluxoagil/models.py
@@ -14,7 +14,8 @@ class Graduation(Base):
     workload_in_hours = Column(Integer)
 
     def __repr__(self) -> str:
-        return f'Graduation: {self.name}'
+        return f'Graduation: {self.name} ({self.id})'
+
 
 class Curriculum(Base):
     __tablename__ = 'curriculum'
@@ -24,4 +25,23 @@ class Curriculum(Base):
     graduation_id = Column(String, ForeignKey('graduation.id'), nullable=False)
 
     def __repr__(self) -> str:
-        return f'Graduation: {self.name}'
+        return f'Currículo: {self.name} ({self.id})'
+
+
+class Course(Base):
+    __tablename__ = 'course'
+    id = Column(String, primary_key=True)
+    name = Column(String(50))
+    description = Column(String(50))
+    credits = Column(Integer)
+    workload_in_hours = Column(Integer)
+
+    def __repr__(self) -> str:
+        return f'Course: {self.name} ({self.id})'
+
+
+class CurriculumContainsCourse(Base):
+    __tablename__ = 'curriculum_contains_course'
+    name = Column(String(50))
+    curriculum_id = Column(String, ForeignKey('curriculum.id'), nullable=False, primary_key=True)
+    course_id = Column(String, ForeignKey('course.id'), nullable=False, primary_key=True)
