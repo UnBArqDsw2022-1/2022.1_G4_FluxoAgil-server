954597f93c902e1cae29c92f31e70b28e0fe5107
Matheus Pinheiro <matheuscvp12@gmail.com>
2022-08-27 14:47:48 -0300

    chore: adiciona tabelas faltantes em models.py

--- fluxoagil/models.py
+++ fluxoagil/models.py
@@ -46,3 +46,30 @@ class CurriculumContainsCourse(Base):
     name = Column(String(50))
     curriculum_id = Column(String, ForeignKey('curriculum.id'), nullable=False, primary_key=True)
     course_id = Column(String, ForeignKey('course.id'), nullable=False, primary_key=True)
+
+class CoursePreRequisite(Base):
+    __tablename__ = 'course_pre_requisite'
+    course_id = Column(String, ForeignKey('course.id'), nullable=False, primary_key=True)
+    pre_requisite_id = Column(String,  ForeignKey('course.id'), nullable=False, primary_key=True)
+    pre_requisite = Column(String(50), nullable=False)
+
+    def __repr__(self) -> str:
+        return f'Pre-Requisite: {self.pre_requisite} ({self.pre_requisite_id})'
+
+class CourseCoRequisite(Base):
+    __tablename__ = 'course_co_requisite'
+    course_id = Column(String, ForeignKey('course.id'), nullable=False, primary_key=True)
+    co_requisite_id = Column(String, ForeignKey('course.id'), nullable=False, primary_key=True)
+    co_requisite = Column(String(50), nullable=False)
+
+    def __repr__(self) -> str:
+        return f'Co-Requisite: {self.co_requisite} ({self.co_requisite_id})'
+
+class CourseEquivalentTo(Base):
+    __tablename__ = 'course_equivalent_to'
+    course_id = Column(String, ForeignKey('course.id'), nullable=False, primary_key=True)
+    equivalente_to_id = Column(String, ForeignKey('course.id'), nullable=False, primary_key=True)
+    equivalent_to = Column(String(50), nullable=False)
+
+    def __repr__(self) -> str:
+        return f'Equivalent To: {self.equivalent_to} ({self.equivalente_to_id})'
