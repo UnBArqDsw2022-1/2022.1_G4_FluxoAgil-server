d8ae5c153be695c2058fac3eccbff1e60317d044
Yudi Yamane <yudiyamane@gmail.com>
2022-08-30 15:20:12 -0300

    refactor: informações da conexão são lidas do arquivo .env

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,6 @@ RUN apt-get install ffmpeg libsm6 libxext6 libpq-dev gcc -y
 WORKDIR /fluxoagil
 
 COPY . /fluxoagil
-RUN pip install -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 
 CMD ["sh", "scripts/start.sh"]
--- fluxoagil/db/connection.py
+++ fluxoagil/db/connection.py
@@ -1,6 +1,17 @@
-
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
-engine = create_engine('postgresql+psycopg2://postgres:password@fluxoagil-db:5432/fluxoagil_db', echo=True, future=True)
+
+# Na formatação dessa string existem um símbolos especiais. Para saber o porquê
+# deles, dê uma olhada na documentação:
+# https://docs.sqlalchemy.org/en/14/core/engines.html#engine-configuration
+connection_string = 'postgresql+psycopg2://'
+connection_string += f'{os.getenv("POSTGRES_USER")}'
+connection_string += f':{os.getenv("POSTGRES_PASSWORD")}'
+connection_string += f'@{os.getenv("POSTGRES_HOST")}'
+connection_string += f':{os.getenv("POSTGRES_PORT")}'
+connection_string += f'/{os.getenv("POSTGRES_DB")}'
+
+engine = create_engine(connection_string, echo=True, future=True)
