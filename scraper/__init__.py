de26ffa9dc0e3bf60f647c17c5566277dfe977b6
Irwin Schmitt <irwinschmitt@gmail.com>
2022-09-07 18:35:44 -0300

    feat: json

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -30,6 +30,8 @@ async def get_programs(programs_link: str):
 
         programs.append(program)
 
+    await browser.close()
+
     return programs
 
 
@@ -58,9 +60,64 @@ async def get_valid_curricula(curricula_link: str):
 
         valid_curricula_id.append(curriculum_id)
 
+    await browser.close()
+
     return valid_curricula_id
+
+
+async def get_curricula_link(graduation_url: str):
+    browser = await launch()
+    page = await browser.newPage()
+    await page.goto(graduation_url)
+
+    # curricula_page_function = ".li-menu:nth-of-type(2) .sub-menu li:nth-of-type(1) a"
+    curricula_link_page_function = '() => document.querySelector(".li-menu:nth-of-type(2) .sub-menu li:nth-of-type(1) a").href'
+    curricula_link = await page.evaluate(curricula_link_page_function)
+
+    await browser.close()
+
+    return curricula_link
+
+
+async def get_curricula(graduation_url: str):
+    curricula_link = await get_curricula_link(graduation_url)
+
+    browser = await launch(headless=False)
+    page = await browser.newPage()
+    await page.goto(curricula_link)
+
+    buttons_selector = "#table_lt tr.linha_par a:nth-of-type(2), #table_lt tr.linha_impar a:nth-of-type(2)"
+    buttons_elements = await page.querySelectorAll(buttons_selector)
+
+    curricula = []
+
+    for index in range(len(buttons_elements)):
+        buttons_selector = "#table_lt tr.linha_par a:nth-of-type(2), #table_lt tr.linha_impar a:nth-of-type(2)"
+        buttons_elements = await page.querySelectorAll(buttons_selector)
+
+        await buttons_elements[index].click()
+        await page.waitForNavigation()
+
+        curriculum_id_fn = '() => document.querySelector("table.formulario tbody tr:nth-of-type(1) td").innerText'
+        curriculum_id = await page.evaluate(curriculum_id_fn)
+
+        curriculum = {
+            "id": curriculum_id
+        }
+
+        curricula.append(curriculum)
+
+        await page.goBack()
+        await page.waitForNavigation()
+
+    return curricula
+
 
 unb_programs_url = "https://sig.unb.br/sigaa/public/curso/lista.jsf?nivel=G&aba=p-graduacao"
 programs = asyncio.run(get_programs(unb_programs_url))
 
-print(programs)
+# limitar pra engenharia de software no mvp
+esw_program_link = next(
+    filter(lambda g: g["title"] == "ENGENHARIA DE SOFTWARE", programs), None)["program_link"]
+
+curricula = asyncio.run(get_curricula(esw_program_link))
